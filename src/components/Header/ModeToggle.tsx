e279173d707b7a34e976515d46686b0655449aa3
thisisatulkumar <theatulkumar11@gmail.com>
2026-02-10 21:32:05 +0530

    feat: add ModeToggle component for theme switching

---
+++ src/components/Header/ModeToggle.tsx
@@ -0,0 +1,56 @@
+"use client"
+
+import { useTheme } from "next-themes"
+
+import { Button } from "@/components/ui/button"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+import { 
+    Moon, 
+    Sun 
+} from "lucide-react"
+
+const ModeToggle = () => {
+    const { setTheme } = useTheme();
+
+    return (
+        <DropdownMenu>
+
+            {/* Mode Icon */}
+            <DropdownMenuTrigger asChild>
+                <Button 
+                    variant="outline" 
+                    size="icon"
+                >
+                    <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+
+                    <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+                    
+                    <span className="sr-only">Toggle theme</span>
+                </Button>
+            </DropdownMenuTrigger>
+
+            {/* Select Mode Dropdown */}
+            <DropdownMenuContent align="end">
+                <DropdownMenuItem onClick={() => setTheme("light")}>
+                    Light
+                </DropdownMenuItem>
+
+                <DropdownMenuItem onClick={() => setTheme("dark")}>
+                    Dark
+                </DropdownMenuItem>
+                
+                <DropdownMenuItem onClick={() => setTheme("system")}>
+                    System
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
+
+export default ModeToggle
