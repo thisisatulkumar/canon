02ac070580a7e4ee4869fff06bce9013ddd8da89
thisisatulkumar <theatulkumar11@gmail.com>
2026-02-10 21:40:19 +0530

    feat: add Header component with page title and mode toggle

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import ModeToggle from "./ModeToggle"
+
+import { SidebarTrigger } from "@/components/ui/sidebar"
+import { Separator } from "@/components/ui/separator"
+
+import { useRouteSegment } from "@/hooks/useRouteSegment"
+
+import { deSlufigy } from "@/utils"
+
+interface HeaderProps {
+    notFound?: boolean;
+}
+
+const Header = ({ notFound }: HeaderProps) => {
+    const segment = useRouteSegment();
+    const title = deSlufigy(segment);
+    
+    return (
+        <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
+            <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
+                <SidebarTrigger className="-ml-1" />
+
+                <Separator
+                    orientation="vertical"
+                    className="mx-2 data-[orientation=vertical]:h-4"
+                />
+
+                {/* Page Title */}
+                <h1 className="text-base font-medium">{notFound ? '404' : title}</h1>
+                
+                {/* Mode Toggle */}
+                <div className="ml-auto flex items-center gap-2">
+                    <ModeToggle />
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
