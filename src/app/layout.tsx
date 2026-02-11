43b06f6efa1db0f5f79976c7f46b1f364ee7f8b3
thisisatulkumar <theatulkumar11@gmail.com>
2026-02-11 18:19:42 +0530

    feat: global layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,69 @@
+import { CSSProperties } from "react";
+
 import { ClerkProvider } from "@clerk/nextjs";
+
+import { ThemeProvider } from "@/lib/theme";
+
+import { shadcn } from "@clerk/themes";
+
+import SyncClerkWithSupabase from "@/components/SyncClerkWithSupabase";
+
+import {
+    SidebarProvider,
+    SidebarInset
+} from "@/components/ui/sidebar";
+
+import AppSidebar from "@/components/Sidebar/AppSidebar";
 
 import "./globals.css";
 
 export default function RootLayout({
-	children,
+    children,
 }: Readonly<{
-	children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-	return (
-		<ClerkProvider>
-			<html lang="en">
-				<body>
-					{children}
-				</body>
-			</html>
-		</ClerkProvider>
-	);
+    return (
+        <ClerkProvider appearance={{ theme: shadcn }}>
+            <html lang="en">
+                <body>
+                    <ThemeProvider
+                        attribute="class"
+                        defaultTheme="system"
+                        enableSystem
+                        disableTransitionOnChange
+                    >
+                        <SyncClerkWithSupabase />
+
+                        <SidebarProvider
+                            style={
+                                {
+                                    "--sidebar-width": "calc(var(--spacing) * 72)",
+                                    "--header-height": "calc(var(--spacing) * 12)",
+                                } as CSSProperties
+                            }
+                        >
+                            <AppSidebar variant="inset" />
+
+                            <SidebarInset>
+                                {/* 
+                                    DO NOT REMOVE THESE COMMENTS: THIS IS HOW YOU SHOULD WRITE YOUR PAGE.TSX 
+
+                                    <>
+                                        <Header />
+
+                                        <div className="flex">
+                                            {actual_stuff}
+                                        </div>
+                                    </>
+                                */}
+                                <main>
+                                    {children}
+                                </main>
+                            </SidebarInset>
+                        </SidebarProvider>
+                    </ThemeProvider>
+                </body>
+            </html>
+        </ClerkProvider>
+    );
 }
