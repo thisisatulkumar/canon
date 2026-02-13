39abfc1685ecc5f320bb0ae21ed0f6aadbe97846
thisisatulkumar <theatulkumar11@gmail.com>
2026-02-13 23:25:05 +0530

    feat: display an intro dialog for first-time users

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,7 +59,7 @@ export default function RootLayout({
                             }
                         >
                             <AppSidebar variant="inset" />
-                            
+
                             <IntroDialog />
 
                             <SidebarInset>
